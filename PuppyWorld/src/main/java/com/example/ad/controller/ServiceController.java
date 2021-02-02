3a92954b5290c58cc4f6609fcde6b923c1e0756f
mrcicil <69301517+mrcicil@users.noreply.github.com>
2021-02-02 08:14:34 +0800

    Edit the service form

--- PuppyWorld/src/main/java/com/example/ad/controller/ServiceController.java
+++ PuppyWorld/src/main/java/com/example/ad/controller/ServiceController.java
@@ -1,14 +1,19 @@
 package com.example.ad.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.ad.domain.Services;
 import com.example.ad.repo.ServiceRepository;
@@ -37,7 +42,14 @@ public class ServiceController {
 	}
 	
 	@RequestMapping(value="/save",method=RequestMethod.POST)
-	public String saveService(@ModelAttribute("service")Services service) {
+	public String saveService(@ModelAttribute("service")Services service, @RequestParam("fileImage") MultipartFile multipartFile) throws IllegalStateException, IOException {
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		System.out.println(fileName);
+		File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
+		multipartFile.transferTo(convFile);
+		byte[] fileContent = FileUtils.readFileToByteArray(convFile);
+		
+		service.setServiceImage(fileContent);
 		Iservice.saveService(service);
 		
 		return "redirect:/";
--- PuppyWorld/src/main/java/com/example/ad/domain/Services.java
+++ PuppyWorld/src/main/java/com/example/ad/domain/Services.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Services {
@@ -17,7 +18,7 @@ public class Services {
 	private String logo;
 	
 	private String serviceName, serviceDescription, serviceDuration, charges;
-	
+	@Lob
 	private byte[] serviceImage;
 	
 	private ReservationType reservationType;
