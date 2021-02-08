173a7c3d228827c53b2eba8f310243becb97b760
mrcicil <69301517+mrcicil@users.noreply.github.com>
2021-02-08 10:35:19 +0800

    Update ServiceController.java

--- PuppyWorld/src/main/java/com/example/ad/controller/ServiceController.java
+++ PuppyWorld/src/main/java/com/example/ad/controller/ServiceController.java
@@ -145,7 +145,7 @@ public class ServiceController {
 		ArrayList<Services> sList = sservice.findAllServices();
 		for (Iterator <Services> iterator = sList.iterator(); iterator.hasNext();) {
 			Services service1 = iterator.next();
-			if(service1.getLocalDate().isEqual(service.getLocalDate())) {
+			if(service1.getLocalDate().isEqual(service.getLocalDate()) & service1.getProvider().getProviderId() == service.getProvider().getProviderId()) {
 				errors.rejectValue("localDate", "exist", "Timeslot exist");
 				break;
 			}
