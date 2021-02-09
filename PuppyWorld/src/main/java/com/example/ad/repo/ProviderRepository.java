6b9a6bd4f4447384b1fe5a596599af3ee40b1a2c
Jiayuan-Hu <Jiayuan-Hu-email@example.com>
2021-02-09 09:47:04 +0800

    Jiayuan
    Service and Product Search function (Java language)

--- PuppyWorld/src/main/java/com/example/ad/repo/ProviderRepository.java
+++ PuppyWorld/src/main/java/com/example/ad/repo/ProviderRepository.java
@@ -1,9 +1,21 @@
 package com.example.ad.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.ad.domain.Provider;
 
 public interface ProviderRepository extends JpaRepository<Provider, Integer> {
+	
+	@Query(value = "SELECT * FROM Provider p "
+			+"WHERE p.provider_description LIKE %:keyword% "
+			+"OR p.provider_name LIKE %:keyword% "
+			+"OR p.reservation_type LIKE %:keyword% "
+			+"OR p.price LIKE %:keyword% ",
+			nativeQuery=true)
+	public ArrayList<Provider> searchProvider(@Param("keyword") String keyword);
 
 }
