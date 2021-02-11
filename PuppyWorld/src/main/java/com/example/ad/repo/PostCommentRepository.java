7e2bcfbd5e7d25566f61dde7f1c3d3d4d9354d4a
joechenyh <e0533420@u.nus.edu>
2021-02-11 13:16:10 +0800

    joe commit part 1

--- PuppyWorld/src/main/java/com/example/ad/repo/PostCommentRepository.java
+++ PuppyWorld/src/main/java/com/example/ad/repo/PostCommentRepository.java
@@ -1,9 +1,17 @@
 package com.example.ad.repo;
 
+import java.util.ArrayList;
+
+import javax.persistence.NamedNativeQuery;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.ad.domain.PostComment;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Integer> {
+	
+	@Query("SELECT p FROM PostComment p WHERE p.post.postId = :postid ORDER by comment_date_time DESC")
+	public ArrayList<PostComment> findPostCommentsbyPostId(int postid);
 
 }
